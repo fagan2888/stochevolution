627dcd95785b45b5ef52923591fb7603a6d3a55b
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2015-07-18 00:05:01 +0900

    Reorganized

--- kmr.py
+++ kmr.py
@@ -2,11 +2,11 @@ from __future__ import division
 import numpy as np
 from scipy.stats import binom
 import matplotlib.pyplot as plt
-from mc_tools import mc_compute_stationary, mc_sample_path
+import quantecon as qe
 
 
-def KMR_2x2_P_simultaneous(N, p, epsilon):
-    P = np.empty((N+1, N+1), dtype=float)
+def kmr_markov_matrix_simultaneous(p, N, epsilon):
+    P = np.empty((N+1, N+1))
     for n in range(N+1):
         P[n, :] = \
             (n/N < p) * binom.pmf(range(N+1), N, epsilon/2) + \
@@ -15,106 +15,132 @@ def KMR_2x2_P_simultaneous(N, p, epsilon):
     return P
 
 
-def KMR_2x2_P_sequential(N, p, epsilon):
-    P = np.zeros((N+1, N+1), dtype=float)
+def kmr_markov_matrix_sequential(p, N, epsilon):
+    P = np.zeros((N+1, N+1))
     P[0, 0], P[0, 1] = 1 - epsilon * (1/2), epsilon * (1/2)
     for n in range(1, N):
-        P[n, n-1] = \
-            (n/N) * (epsilon * (1/2) +
-                     (1 - epsilon) * (((n-1)/(N-1) < p) + ((n-1)/(N-1) == p) * (1/2))
-                     )
-        P[n, n+1] = \
-            ((N-n)/N) * (epsilon * (1/2) +
-                         (1 - epsilon) * ((n/(N-1) > p) + (n/(N-1) == p) * (1/2))
-                         )
+        P[n, n-1] = (n/N) * (
+            epsilon * (1/2) +
+            (1 - epsilon) * (((n-1)/(N-1) < p) + ((n-1)/(N-1) == p) * (1/2))
+        )
+        P[n, n+1] = ((N-n)/N) * (
+            epsilon * (1/2) +
+            (1 - epsilon) * ((n/(N-1) > p) + (n/(N-1) == p) * (1/2))
+        )
         P[n, n] = 1 - P[n, n-1] - P[n, n+1]
     P[N, N-1], P[N, N] = epsilon * (1/2), 1 - epsilon * (1/2)
     return P
 
 
-class KMR_2x2:
+class KMR2x2(object):
 
-    def __init__(self, N, p, epsilon, move='simultaneous'):
+    def __init__(self, p, N, epsilon, revision='simultaneous'):
+
+        self.p, self.N = p, N
         self._epsilon = epsilon
-        self.N, self.p, self.move = N, p, move
-        self.set_P()
+        self._mc = None
 
-    def get_epsilon(self):
+        if revision == 'simultaneous':
+            self.kmr_markov_matrix = kmr_markov_matrix_simultaneous
+        elif revision == 'sequential':
+            self.kmr_markov_matrix = kmr_markov_matrix_sequential
+        else:
+            raise ValueError
+
+    @property
+    def epsilon(self):
         return self._epsilon
 
-    def set_epsilon(self, new_value):
-        self._epsilon = new_value
-        self.set_P()
+    @epsilon.setter
+    def epsilon(self, value):
+        self._epsilon = value
+        self._mc = None
 
-    epsilon = property(get_epsilon, set_epsilon)
+    @property
+    def mc(self):
+        if self._mc is None:
+            P = self.kmr_markov_matrix(self.p, self.N, self.epsilon)
+            self._mc = qe.MarkovChain(P)
+        return self._mc
 
-    def set_P(self):
-        if self.move == 'sequential':
-            self.P = KMR_2x2_P_sequential(self.N, self.p, self._epsilon)
-        else:
-            self.P = KMR_2x2_P_simultaneous(self.N, self.p, self._epsilon)
-
-    def simulate(self, T=100000, x0=0):
+    def simulate(self, ts_length, init=None, num_reps=None):
         """
-        Generates a NumPy array containing a sample path of length T
-        with initial state x0 = 0
+        Simulate the dynamics.
+
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            Length of each simulation.
+
+        init : scalar(int) or array_like(int, ndim=1),
+               optional(default=None)
+            Initial state(s). If None, the initial state is randomly
+            drawn.
+
+        num_reps : scalar(int), optional(default=None)
+            Number of simulations. Relevant only when init is a scalar
+            or None.
+
+        Returns
+        -------
+        ndarray(int, ndim=1 or 2)
+            Array containing the sample path(s), of shape (ts_length,)
+            if init is a scalar (integer) or None and num_reps is None;
+            of shape (k, ts_length) otherwise, where k = len(init) if
+            init is an array_like, otherwise k = num_reps.
+
         """
-        self.s = mc_sample_path(self.P, x0, T)
+        return self.mc.simulate(ts_length, init, num_reps)
 
-    def get_sample_path(self):
-        return self.s
+    def plot_sample_path(self, x=None, ts_length=10**4, init=None,
+                         ax=None, show=True):
+        if x is None:
+            x = self.simulate(ts_length, init)
 
-    def plot_sample_path(self, ax=None, show=True):
         if show:
             fig, ax = plt.subplots()
-        ax.plot(self.s, alpha=0.5)
+        ax.plot(x, alpha=0.5)
         ax.set_ylim(0, self.N)
         ax.set_title(r'Sample path: $\varepsilon = {0}$'.format(self._epsilon))
-        ax.set_xlabel('time')
-        ax.set_ylabel('state space')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('State')
         if show:
             plt.show()
 
-    def plot_emprical_dist(self, ax=None, show=True):
+    def plot_emprical_dist(self, x=None, ts_length=10**4, init=None,
+                           ax=None, show=True):
+        if x is None:
+            x = self.simulate(ts_length, init)
+
         if show:
             fig, ax = plt.subplots()
-        hist, bins = np.histogram(self.s, self.N+1)
+        hist, bins = np.histogram(x, self.N+1)
         ax.bar(range(self.N+1), hist, align='center')
-        ax.set_title(r'Emprical distribution: $\varepsilon = {0}$'.format(self._epsilon))
+        ax.set_title(r'Emprical distribution: $\varepsilon = {0}$'
+                     .format(self.epsilon))
         ax.set_xlim(-0.5, self.N+0.5)
-        ax.set_xlabel('state space')
-        ax.set_ylabel('frequency')
+        ax.set_xlabel('State')
+        ax.set_ylabel('Frequency')
         if show:
             plt.show()
 
-    def compute_stationary_dist(self):
+    @property
+    def stationary_dist(self):
         """
-        Generates a NumPy array containing the stationary distribution
-        """
-        self.mu = mc_compute_stationary(self.P)
+        Return an array containing the stationary distribution
 
-    def get_stationary_dist(self):
-        return self.mu
+        """
+        return self.mc.stationary_distributions[0]
 
     def plot_stationary_dist(self, ax=None, show=True):
         if show:
             fig, ax = plt.subplots()
-        ax.bar(range(self.N+1), self.mu, align='center')
+        ax.bar(range(self.N+1), self.stationary_dist, align='center')
         ax.set_xlim(-0.5, self.N+0.5)
         ax.set_ylim(0, 1)
-        ax.set_title(r'Stationary distribution: $\varepsilon = {0}$'.format(self._epsilon))
-        ax.set_xlabel('state space')
-        ax.set_ylabel('probability')
+        ax.set_title(r'Stationary distribution: $\varepsilon = {0}$'
+                     .format(self.epsilon))
+        ax.set_xlabel('State')
+        ax.set_ylabel('Probability')
         if show:
             plt.show()
-
-
-if __name__ == '__main__':
-    p = 1/3
-    N = 10
-    epsilon = 0.03
-    T = 300000
-
-    kmr = KMR_2x2(N, p, epsilon)
-    kmr.simulate(T)
-    kmr.plot_sample_path()
